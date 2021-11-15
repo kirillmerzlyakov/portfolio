e4956c3fc2f36ef61a78f3a0630ca7d2543c172c
kmerzlyakov <kmerzlyakov@skbkontur.ru>
2021-11-15 23:44:58 +0500

    поправил шрифты

--- src/mainPage/mainPage.tsx
+++ src/mainPage/mainPage.tsx
@@ -4,20 +4,28 @@ import "./mainPage.css";
 export const MainPage: React.FunctionComponent = (props) => (
   <div className="main-page">
     <div className="first-main-page">
-      <div className="menu-main-page">
-        <div className="menu-main-page-text">
-          Дарина Лачихина
-          <br />
-          дизайнер
-        </div>
-      </div>
-      <div className="title-main-page">
-        <div className="title-text-wrapper">
-          <div className="text-1">DIGITAL DESIGNER</div>
-          <div className="text-2">/INSIGHT</div>
-        </div>
-        <div className="text-3">PORTFOLIO</div>
-      </div>
+      {renderMenuBlock()}
+      {renderTitleBlock()}
     </div>
+  </div>
+);
+
+const renderMenuBlock = () => (
+  <div className="menu-main-page">
+    <div className="menu-main-page-text">
+      Дарина Лачихина
+      <br />
+      дизайнер
+    </div>
+  </div>
+);
+
+const renderTitleBlock = () => (
+  <div className="title-main-page">
+    <div className="title-text-wrapper">
+      <div className="text-1">DIGITAL DESIGNER</div>
+      <div className="text-2">/INSIGHT</div>
+    </div>
+    <div className="text-3">PORTFOLIO</div>
   </div>
 );
