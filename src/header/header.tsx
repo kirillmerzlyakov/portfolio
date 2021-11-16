36444d245e7e2b78d333ceafccd2a6c28b994a43
kmerzlyakov <kmerzlyakov@skbkontur.ru>
2021-11-16 21:06:47 +0500

    FC

--- src/header/header.tsx
+++ src/header/header.tsx
@@ -1,29 +1,20 @@
 import React from "react";
 import "./header.css";
 import { Link } from "react-router-dom";
-export class Header extends React.Component {
-  componentDidUpdate() {
-    console.log("rerender");
-  }
 
-  render() {
-    return (
-      <div className="header">
-        <div className="navigation">
-          {window.location.pathname !== "/" && renderBackArrow()}
-        </div>
-        <div className="header-main">Дарина Лачихина</div>
-        <div className="header-buttons">
-          <Link className="header-button" to="/skinscan">
-            Портфолио
-          </Link>
-          <div className="header-button">Обо&nbsp;мне</div>
-          <div className="header-button">Контакты</div>
-        </div>
-      </div>
-    );
-  }
-}
+export const Header: React.FC = (props) => (
+  <div className="header">
+    <div className="navigation">{renderBackArrow()}</div>
+    <div className="header-main">Дарина Лачихина</div>
+    <div className="header-buttons">
+      <Link className="header-button" to="/skinscan">
+        Портфолио
+      </Link>
+      <div className="header-button">Обо&nbsp;мне</div>
+      <div className="header-button">Контакты</div>
+    </div>
+  </div>
+);
 
 const renderBackArrow = () => (
   <Link className="navigate-link" to="/">
