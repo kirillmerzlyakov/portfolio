4d690e2b14118568d5efecd8582496f71da95848
kmerzlyakov <kmerzlyakov@skbkontur.ru>
2021-11-22 01:45:04 +0500

    добавил brics

--- src/projects/brics/brics.tsx
+++ src/projects/brics/brics.tsx
@@ -2,29 +2,38 @@ import React from "react";
 import { Menu } from "../../components/menu/menu";
 import "./brics.css";
 
+const TEXT_BLOCK = [
+  "Для входа в свой кошелек необходимо создать и подтвердить мнемони-ческую фразу. Для этого я придумал интерактивный сценарий.",
+];
+
 export const Brics: React.FunctionComponent = (props) => (
   <div className="content-skinscan">
     <Menu
-      name={"BRICS"}
+      name={"BRICS competition"}
       city={"Екатеринбург"}
       year={"2021"}
       description={
         "блабалбалбалбалбалаблабла блаблб албафыолврф олырвлдфорывлфоыролдфы рвфолыврфолдвр"
       }
     />
-    {[2, 3, 4, 5, 6].map((a) => getImageBlock(a.toString()))}
-    
+    {renderImageBlock("1")}
+
+    <div className="text-wrapper">
+      {renderTextBlock(TEXT_BLOCK[0])}
+      {renderTextBlock(TEXT_BLOCK[0])}
+    </div>
+    {renderImageBlock("2")}
   </div>
 );
 
-function getImageBlock(number: string) {
+function renderTextBlock(text: string) {
+  return <div className="text-block">{text}</div>;
+}
+
+function renderImageBlock(number: string) {
   return (
     <div className="img-wrapper">
-      <img
-        className="image"
-        src={`./brics/${number}.jpg`}
-        alt="картинка"
-      ></img>
+      <img className="image" src={`./brics/${number}.jpg`} alt="картинка"></img>
     </div>
   );
 }
