ce0c4bc0639f116fdc05e3f29a3c20bb2e1b9a5b
kmerzlyakov <kmerzlyakov@skbkontur.ru>
2021-11-26 01:20:03 +0500

    убираю кнопку назад на главном экране

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,28 +1,29 @@
 import React from "react";
 import cn from "./header.module.less";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-export class Header extends React.Component {
-  render() {
-    return (
-      <div className={cn["header"]}>
-        <div className={cn["navigation"]}>{renderBackArrow()}</div>
-        <div className={cn["main"]}>Дарина Лачихина</div>
-        <div className={cn["buttons"]}>
-          <Link className={cn["button"]} to="/">
-            Портфолио
+export const Header: React.FC = (props) => {
+  const { pathname } = useLocation();
+  return (
+    <div className={cn["header"]}>
+      <div className={cn["navigation"]}>
+        {pathname !== "/" && renderBackArrow()}
+      </div>
+      <div className={cn["main"]}>Дарина Лачихина</div>
+      <div className={cn["buttons"]}>
+        <Link className={cn["button"]} to="/">
+          Портфолио
+        </Link>
+        <div className={cn["button"]}>Обо&nbsp;мне</div>
+        <div className={cn["button"]}>
+          <Link className={cn["button"]} to="/contacts">
+            Контакты
           </Link>
-          <div className={cn["button"]}>Обо&nbsp;мне</div>
-          <div className={cn["button"]}>
-            <Link className={cn["button"]} to="/contacts">
-              Контакты
-            </Link>
-          </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 const renderBackArrow = () => (
   <Link className={cn["navigate-link"]} to="/">
