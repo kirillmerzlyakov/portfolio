ab67892d2b6852d9b28318acec09be00ea9be1f4
kmerzlyakov <kmerzlyakov@skbkontur.ru>
2021-12-01 00:17:24 +0500

    типограф

--- src/projects/skinscan.tsx
+++ src/projects/skinscan.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Menu } from "../components/menu/menu";
-import { renderImageBlock, renderTextBlock, renderVideoBlock } from "./common";
+import { renderImageBlock, renderVideoBlock } from "./common";
 import cn from "./projects.module.less";
 
 const VIDEO_SOURCES: { [key: string]: string } = {
@@ -11,19 +11,9 @@ const VIDEO_SOURCES: { [key: string]: string } = {
     "https://player.vimeo.com/video/645741574?h=f4882dc24c&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
 };
 
-const TEXT_BLOCK = [
-  "В январе 2021 я получила запрос на редизайн готового продукта Act-Contract (старое название). Интерфейс сервиса устарел, не выполнял все задачи клиента, фирменный стиль хотелось освежить. ",
-  "Вместе с заказчиком мы провели полный ребрендинг и сменили название. Я выбрала новые фирменные цвета и шрифты, отрисовала логотип и иллюстрации. ",
-  "Основной задачей было сделать новый дизайн сервиса удобным и интуитивно понятным с минимальным пользовательским путем. Кроме того, мы хотели внедрить в него новые функции, не меняя старые прототипы глобально. ",
-  "Вот, что получилось. Панель управления элементами справа. Слева - навигация по документу и панель добавления элементов, с возможностью подгружать новые и использовать ранее загруженные. ",
-  "Мы спроектировали интерфейс оплаты и оформления подписки на сервис с интерактивной картой, в которую нужно ввести данные. Такое решение нативно и удобно.",
-  "Далее нами с клиентом было принято решение по разработке продающего лендинга. Продукт завоевал достаточно крупную аудиторию на российском рынке, теперь мы были готовы выйти на международный. ",
-  "Для каждого проекта обязательно прорабатываю взаимодействия и анимацию — это неотъемлемая часть хорошего пользовательского опыта.",
-];
-
 const FOLDER_NAME = "skinscan";
 
-export const SkinScan: React.FunctionComponent = (props) => {
+export const SkinScan: React.FunctionComponent = () => {
   return (
     <div className={cn["projects-content"]}>
       <Menu
@@ -37,20 +27,58 @@ export const SkinScan: React.FunctionComponent = (props) => {
       />
       {renderVideoBlock("Act-Contract-0", VIDEO_SOURCES["Act-Contract-0"])}
       <div className={cn["text-wrapper"]}>
-        {renderTextBlock(TEXT_BLOCK[0])}
-        {renderTextBlock(TEXT_BLOCK[1])}
+        <div className={cn["text-block"]}>
+          В&nbsp;январе 2021&nbsp;я получила запрос на&nbsp;редизайн готового
+          продукта Act-Contract (старое название). Интерфейс сервиса устарел,
+          не&nbsp;выполнял все задачи клиента, фирменный стиль хотелось
+          освежить.
+        </div>
+        <div className={cn["text-block"]}>
+          Вместе с&nbsp;заказчиком мы&nbsp;провели полный ребрендинг
+          и&nbsp;сменили название. Я&nbsp;выбрала новые фирменные цвета
+          и&nbsp;шрифты, отрисовала логотип и&nbsp;иллюстрации.
+        </div>
       </div>
       {[11, 12].map((a) => renderImageBlock(FOLDER_NAME, a))}
       <div className={cn["text-wrapper"]}>
-        {renderTextBlock(TEXT_BLOCK[2])}
-        {renderTextBlock(TEXT_BLOCK[3])}
+        <div className={cn["text-block"]}>
+          Основной задачей было сделать новый дизайн сервиса удобным
+          и&nbsp;интуитивно понятным с&nbsp;минимальным пользовательским путем.
+          Кроме того, мы&nbsp;хотели внедрить в&nbsp;него новые функции,
+          не&nbsp;меняя старые прототипы глобально.
+        </div>
+        <div className={cn["text-block"]}>
+          Вот, что получилось. Панель управления элементами справа.
+          Слева&nbsp;&mdash; навигация по&nbsp;документу и&nbsp;панель
+          добавления элементов, с&nbsp;возможностью подгружать новые
+          и&nbsp;использовать ранее загруженные.
+        </div>
       </div>
       {[8, 9].map((a) => renderImageBlock(FOLDER_NAME, a))}
-      <div className={cn["text-wrapper"]}>{renderTextBlock(TEXT_BLOCK[4])}</div>
+      <div className={cn["text-wrapper"]}>
+        <div className={cn["text-block-wide"]}>
+          Мы&nbsp;спроектировали интерфейс оплаты и&nbsp;оформления подписки
+          на&nbsp;сервис с&nbsp;интерактивной картой, в&nbsp;которую нужно
+          ввести данные. Такое решение нативно и&nbsp;удобно.
+        </div>
+      </div>
       {renderImageBlock(FOLDER_NAME, 10)}
-      <div className={cn["text-wrapper"]}>{renderTextBlock(TEXT_BLOCK[5])}</div>
+      <div className={cn["text-wrapper"]}>
+        <div className={cn["text-block-wide"]}>
+          Далее нами с&nbsp;клиентом было принято решение по&nbsp;разработке
+          продающего лендинга. Продукт завоевал достаточно крупную аудиторию
+          на&nbsp;российском рынке, теперь мы&nbsp;были готовы выйти
+          на&nbsp;международный.
+        </div>
+      </div>
       {[2, 3, 4, 5, 6, 7].map((a) => renderImageBlock(FOLDER_NAME, a))}
-      <div className={cn["text-wrapper"]}>{renderTextBlock(TEXT_BLOCK[6])}</div>
+      <div className={cn["text-wrapper"]}>
+        <div className={cn["text-block-wide"]}>
+          Для каждого проекта обязательно прорабатываю взаимодействия
+          и&nbsp;анимацию&nbsp;&mdash; это&nbsp;неотъемлемая часть хорошего
+          пользовательского опыта.
+        </div>
+      </div>
       {renderVideoBlock("Act-Contract-1", VIDEO_SOURCES["Act-Contract-1"])}
       {renderVideoBlock("Act-Contract-2", VIDEO_SOURCES["Act-Contract-2"])}
     </div>
