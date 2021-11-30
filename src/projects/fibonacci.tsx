ce8f701ad710f0dda677482ea18f63e08878dd47
kmerzlyakov <kmerzlyakov@skbkontur.ru>
2021-12-01 00:06:34 +0500

    типограф

--- src/projects/fibonacci.tsx
+++ src/projects/fibonacci.tsx
@@ -1,25 +1,13 @@
 import React from "react";
 import { Menu } from "../components/menu/menu";
-import { renderImageBlock, renderTextBlock, renderVideoBlock } from "./common";
+import { renderImageBlock, renderVideoBlock } from "./common";
 import cn from "./projects.module.less";
 import { Slide } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
 
-const TEXT_BLOCK = [
-  "Fibonacci. Московская компания-бутик, которая создает жилое пространство фокусируясь на качестве объектов недвижимости, быстрых сроках их реализации",
-  "Заказчик имел определенные предпочтения по цветовой палире, стилистике и шрифтам. Поэтому работа началась с составления брендбука и key-visual.",
-  "В течение работы над проектом, я разработала  несколько концепций главной страницы. Ниже представлен финальный вариант, который был передан в верстку.",
-  "Мы утвердили концепцию, в которой основная страница не имеет акцентных блоков и ярких цветов, а для каждого ЖК Fibonacci Group был подобрана своя контрастная насыщенная палитра.",
-  "Прототип-демонстрация работы блока внутренних страниц ЖК Fibonacci Group.",
-  "Для каждого проекта обязательно прорабатываю взаимодействия и анимацию. Во-первых это помогает утвердить сайт с клиентом. Во-вторых упрощает жизнь разработчикам.",
-  "Для внутренних страниц сайта – страниц жилищных комплексов был отрисован общий шаблон.",
-  "Hero block содержит в себе фото и видео комплекса, перелинковку на официальную страницу ЖК и основную инфорацию о проекте. Далее идет описание со слацдерами фото и интерактивная карта с местоположением комплекса.",
-  "Гайдлайн помогает сориентироваться и помогает понять как и когда использовать разные версии логотипа. Вы можете полистать его ниже.",
-  "Для Fibonacci были придуманы несколько версий логотипа и полноценная айдентика: фирменный паттерн, графические элементы и иконки, стиль оформления социальных сетей, постеры и плакаты, уличная реклама, визитные карточки и конверты.",
-];
 const FOLDER_NAME = "fibonacci";
 
-export const Fibonacci: React.FunctionComponent = (props) => (
+export const Fibonacci: React.FunctionComponent = () => (
   <div className={cn["projects-content"]}>
     <Menu
       name={"Fibonacci"}
@@ -32,29 +20,67 @@ export const Fibonacci: React.FunctionComponent = (props) => (
     />
     {renderImageBlock(FOLDER_NAME, 1)}
     <div className={cn["text-wrapper"]}>
-      {renderTextBlock(TEXT_BLOCK[0])}
-      {renderTextBlock(TEXT_BLOCK[1])}
+      <div className={cn["text-block"]}>
+        Fibonacci. Московская компания-бутик, которая создает жилое пространство
+        фокусируясь на&nbsp;качестве объектов недвижимости, быстрых сроках
+        их&nbsp;реализации.
+      </div>
+      <div className={cn["text-block"]}>
+        Заказчик имел определенные предпочтения по&nbsp;цветовой палире,
+        стилистике и&nbsp;шрифтам. Поэтому работа началась с&nbsp;составления
+        брендбука и&nbsp;key-visual.
+      </div>
     </div>
     {renderImageBlock(FOLDER_NAME, 2)}
     <div className={cn["text-wrapper"]}>
-      {renderTextBlock(TEXT_BLOCK[2])}
-      {renderTextBlock(TEXT_BLOCK[3])}
+      <div className={cn["text-block"]}>
+        В&nbsp;течение работы над проектом, я&nbsp;разработала несколько
+        концепций главной страницы. Ниже представлен финальный вариант, который
+        был передан в&nbsp;верстку.
+      </div>
+      <div className={cn["text-block"]}>
+        Мы&nbsp;утвердили концепцию, в&nbsp;которой основная страница
+        не&nbsp;имеет акцентных блоков и&nbsp;ярких цветов, а&nbsp;для каждого
+        ЖК&nbsp;Fibonacci Group был подобрана своя контрастная насыщенная
+        палитра.
+      </div>
     </div>
     {[3, 4, 5, 6].map((i) => renderImageBlock(FOLDER_NAME, i))}
     <div className={cn["text-wrapper"]}>
-      {renderTextBlock(TEXT_BLOCK[4])}
-      {renderTextBlock(TEXT_BLOCK[5])}
+      <div className={cn["text-block-wide"]}>
+        Прототип-демонстрация работы блока внутренних страниц
+        ЖК&nbsp;Fibonacci&nbsp;Group. Для&nbsp;каждого проекта обязательно
+        прорабатываю взаимодействия и&nbsp;анимацию. Во-первых это помогает
+        утвердить сайт с&nbsp;клиентом. Во-вторых упрощает жизнь разработчикам.
+      </div>
     </div>
     {renderVideoBlock("Fibonacci", "https://player.vimeo.com/video/649489917")}
 
     <div className={cn["text-wrapper"]}>
-      {renderTextBlock(TEXT_BLOCK[6])}
-      {renderTextBlock(TEXT_BLOCK[7])}
+      <div className={cn["text-block"]}>
+        Для внутренних страниц сайта&nbsp;&mdash; страниц жилищных комплексов
+        был отрисован общий шаблон.
+      </div>
+      <div className={cn["text-block"]}>
+        Hero block содержит в&nbsp;себе фото и&nbsp;видео комплекса,
+        перелинковку на&nbsp;официальную страницу&nbsp;ЖК и&nbsp;основную
+        инфорацию о&nbsp;проекте. Далее идет описание со&nbsp;слацдерами фото
+        и&nbsp;интерактивная карта с&nbsp;местоположением комплекса.
+      </div>
     </div>
     {[7, 8, 9, 10].map((i) => renderImageBlock(FOLDER_NAME, i))}
     <div className={cn["text-wrapper"]}>
-      {renderTextBlock(TEXT_BLOCK[8])}
-      {renderTextBlock(TEXT_BLOCK[9])}
+      <div className={cn["text-block"]}>
+        Гайдлайн помогает сориентироваться и&nbsp;помогает понять как
+        и&nbsp;когда использовать разные версии логотипа. Вы&nbsp;можете
+        полистать его ниже.
+      </div>
+      <div className={cn["text-block"]}>
+        Для Fibonacci были придуманы несколько версий логотипа
+        и&nbsp;полноценная айдентика: фирменный паттерн, графические элементы
+        и&nbsp;иконки, стиль оформления социальных сетей, постеры
+        и&nbsp;плакаты, уличная реклама, визитные карточки и&nbsp;конверты.
+      </div>
     </div>
     <Slideshow />
   </div>
