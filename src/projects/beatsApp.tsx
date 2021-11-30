abe592a9d6e6b20b1609dfbf9a6fd3bcf4373b5d
kmerzlyakov <kmerzlyakov@skbkontur.ru>
2021-11-30 23:18:33 +0500

    типограф

--- src/projects/beatsApp.tsx
+++ src/projects/beatsApp.tsx
@@ -1,13 +1,7 @@
 import React from "react";
 import { Menu } from "../components/menu/menu";
-import { renderImageBlock, renderTextBlock } from "./common";
+import { renderImageBlock } from "./common";
 import cn from "./projects.module.less";
-
-const TEXT_BLOCK = [
-  "BeatsApp – мобильное приложене для прослушивания музыки, которое я разрабатывала для стартапа из Израиля.",
-  "Ниже представлены несколько экранов, которые раскрывают суть проекта. Приложение еще находится в разработке.",
-
-];
 
 const FOLDER_NAME = "beatsApp";
 
@@ -23,8 +17,14 @@ export const BeatsApp: React.FunctionComponent = (props) => (
     />
     {renderImageBlock(FOLDER_NAME, 1)}
     <div className={cn["text-wrapper"]}>
-      {renderTextBlock(TEXT_BLOCK[0])}
-      {renderTextBlock(TEXT_BLOCK[1])}
+      <div className={cn["text-block"]}>
+        BeatsApp&nbsp;&mdash; мобильное приложене для прослушивания музыки,
+        которое я&nbsp;разрабатывала для стартапа из&nbsp;Израиля.
+      </div>
+      <div className={cn["text-block"]}>
+        Ниже представлены несколько экранов, которые раскрывают суть проекта.
+        Приложение еще находится в&nbsp;разработке.
+      </div>
     </div>
     {[2, 3].map((i) => renderImageBlock(FOLDER_NAME, i))}
   </div>
