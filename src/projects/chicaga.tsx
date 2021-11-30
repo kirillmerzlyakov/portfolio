9cb8d5860cc99838ad4f0dd13da2fd3889886854
kmerzlyakov <kmerzlyakov@skbkontur.ru>
2021-11-30 23:57:11 +0500

    типограф

--- src/projects/chicaga.tsx
+++ src/projects/chicaga.tsx
@@ -1,19 +1,11 @@
 import React from "react";
 import { Menu } from "../components/menu/menu";
-import { renderImageBlock, renderTextBlock } from "./common";
+import { renderImageBlock } from "./common";
 import cn from "./projects.module.less";
 
-const TEXT_BLOCK = [
-  "В 2020 году мне предложили заняться доработкой и редизайном сайта для сети школ английского языка Chicaga.",
-  "Конечно, всегда интереснее работать над проектами без дизайна, которые нужно создавать с нуля. Но стилистика, заданная командой Chicaga показалась мне необычной и привлекательной, поэтому я с удовольствием согласилась.",
-  "Сайт состоит из 40 веб-страниц. Для каждой отрисованы адптив и по несколько задизайненных экранов для всех состояний интерактивных блоков.",
-  "На картинке изображены только мобильные версии сайта и часть библиотеки компонентов. Это один из самых больших проектов, которые я делала.",
-  "Еще кое-какие экраны, которые кажутся мне симпатичными:",
-  "Далее представлена часть UI-кита, с которой, я и начала работу. Выбрали и утвердили цветовую гамму, шрифы, проработали кнопки, инпуты и дропдауны.",
-];
 const FOLDER_NAME = "chicaga";
 
-export const Chicaga: React.FunctionComponent = (props) => (
+export const Chicaga: React.FunctionComponent = () => (
   <div className={cn["projects-content"]}>
     <Menu
       name={"CHICAGA"}
@@ -26,18 +18,44 @@ export const Chicaga: React.FunctionComponent = (props) => (
     />
     {renderImageBlock(FOLDER_NAME, 1)}
     <div className={cn["text-wrapper"]}>
-      {renderTextBlock(TEXT_BLOCK[0])}
-      {renderTextBlock(TEXT_BLOCK[1])}
+      <div className={cn["text-block"]}>
+        В&nbsp;2020 году мне предложили заняться доработкой и&nbsp;редизайном
+        сайта для сети школ английского языка Chicaga.
+      </div>
+      <div className={cn["text-block"]}>
+        Конечно, всегда интереснее работать над проектами без дизайна, которые
+        нужно создавать с&nbsp;нуля. Но&nbsp;стилистика, заданная командой
+        Chicaga показалась мне необычной и&nbsp;привлекательной, поэтому
+        я&nbsp;с&nbsp;удовольствием согласилась.
+      </div>
     </div>
     {[2, 3, 4, 5].map((i) => renderImageBlock(FOLDER_NAME, i))}
     <div className={cn["text-wrapper"]}>
-      {renderTextBlock(TEXT_BLOCK[2])}
-      {renderTextBlock(TEXT_BLOCK[3])}
+      <div className={cn["text-block"]}>
+        Сайт состоит из&nbsp;40&nbsp;веб-страниц. Для каждой отрисованы адптив
+        и&nbsp;по&nbsp;несколько задизайненных экранов для всех состояний
+        интерактивных блоков.
+      </div>
+      <div className={cn["text-block"]}>
+        На&nbsp;картинке изображены только мобильные версии сайта и&nbsp;часть
+        библиотеки компонентов. Это один из&nbsp;самых больших проектов, которые
+        я&nbsp;делала.
+      </div>
     </div>
     {renderImageBlock(FOLDER_NAME, 6)}
-    <div className={cn["text-wrapper"]}>{renderTextBlock(TEXT_BLOCK[4])}</div>
+    <div className={cn["text-wrapper"]}>
+      <div className={cn["text-block-wide"]}>
+        Еще кое-какие экраны, которые кажутся мне симпатичными:
+      </div>
+    </div>
     {[7, 8, 9].map((i) => renderImageBlock(FOLDER_NAME, i))}
-    <div className={cn["text-wrapper"]}>{renderTextBlock(TEXT_BLOCK[5])}</div>
+    <div className={cn["text-wrapper"]}>
+      <div className={cn["text-block-wide"]}>
+        Далее представлена часть UI-кита, с&nbsp;которой, я&nbsp;и&nbsp;начала
+        работу. Выбрали и&nbsp;утвердили цветовую гамму, шрифы, проработали
+        кнопки, инпуты и&nbsp;дропдауны.
+      </div>
+    </div>
     {[10, 11, 12].map((i) => renderImageBlock(FOLDER_NAME, i))}
   </div>
 );
