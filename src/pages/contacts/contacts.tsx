abf1d44490e57a80fee342c54e20fa9ee49a3bb8
kmerzlyakov <kmerzlyakov@skbkontur.ru>
2021-12-01 23:52:30 +0500

    рефакторинг

--- src/pages/contacts/contacts.tsx
+++ src/pages/contacts/contacts.tsx
@@ -3,10 +3,10 @@ import avatar from "../media/avatar.png";
 import contacts from "../media/contacts.jpg";
 
 export const Contacts = () => (
-  <div className={cn["main-page"]}>
-    <div className={cn["first-block-wrapper"]}>
-      <div className={cn["menu"]}>
-        <img className={cn["avatar"]} src={avatar} alt="картинка"></img>
+  <div className={cn.mainPage}>
+    <div className={cn.firstBlockWrapper}>
+      <div className={cn.menu}>
+        <img className={cn.avatar} src={avatar} alt="картинка"></img>
       </div>
       {renderTitulBlock()}
     </div>
@@ -14,13 +14,13 @@ export const Contacts = () => (
 );
 
 const renderTitulBlock = () => (
-  <div className={cn["title"]}>
-    <div className={cn["wrapper"]}>
-      <img className={cn["contacts"]} src={contacts} alt="картинка"></img>
-      <div className={cn["contact-block"]}>
-        <div className={cn["contact-block1"]}>Напишите мне:</div>
-        <div className={cn["contact-block2"]}>e. lachikhina2017@yandex.ru</div>
-        <a className={cn["phone"]} href="tel:79321239426">
+  <div className={cn.title}>
+    <div className={cn.wrapper}>
+      <img className={cn.contacts} src={contacts} alt="картинка"></img>
+      <div className={cn.contactBlock}>
+        <div className={cn.contactBlock1}>Напишите мне:</div>
+        <div className={cn.contactBlock2}>e. lachikhina2017@yandex.ru</div>
+        <a className={cn.phone} href="tel:79321239426">
           t. +7 932 123 9426
         </a>
       </div>
