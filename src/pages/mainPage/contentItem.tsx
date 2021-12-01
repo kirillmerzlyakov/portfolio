79d5e1b7bf7b06f7b504e4b216cc1a6bf2d6a7c4
kmerzlyakov <kmerzlyakov@skbkontur.ru>
2021-12-01 23:43:19 +0500

    поправил стили

--- src/pages/mainPage/contentItem.tsx
+++ src/pages/mainPage/contentItem.tsx
@@ -13,18 +13,18 @@ export const ContentItem: React.FunctionComponent<Props> = (props) => {
   const [isHovering, setIsHovering] = useState(false);
   return (
     <div
-      className={cn["content-item"]}
+      className={cn.contentItem}
       onMouseOver={() => setIsHovering(true)}
       onMouseOut={() => setIsHovering(false)}
     >
-      <span className={cn["header"]}>
-        <Link className={cn["link"]} to={props.route}>
-          <span className={cn["header1"]}>{props.name}</span>
+      <span className={cn.header}>
+        <Link className={cn.link} to={props.route}>
+          <span className={cn.header1}>{props.name}</span>
         </Link>
       </span>
       <Link to={props.route}>
         <img
-          className={cn["item-image"]}
+          className={cn.itemImage}
           src={isHovering ? props.gifSrc : props.imgSrc}
           alt="картинка"
         ></img>
--- src/pages/mainPage/mainPage.tsx
+++ src/pages/mainPage/mainPage.tsx
@@ -2,28 +2,24 @@ import React from "react";
 import cn from "./mainPage.module.less";
 import { ContentItem } from "./contentItem";
 import titul from "../media/titul.jpg";
-
 import skinscanImg from "../media/tiles/skinscan.jpg";
 import skinscanGif from "../media/tiles/skinscan.gif";
-
 import bricsImg from "../media/tiles/brics.jpg";
 import bricsGif from "../media/tiles/brics.gif";
-
 import beatsAppImg from "../media/tiles/beatsApp.jpg";
 import beatsAppGif from "../media/tiles/beatsApp.gif";
-
 import fibonacciImg from "../media/tiles/fibonacci.jpg";
 import fibonacciGif from "../media/tiles/fibonacci.gif";
 
 export const MainPage: React.FunctionComponent = (props) => (
-  <div className={cn["main-page"]}>
-    <div className={cn["first-block-wrapper"]}>
+  <div className={cn.mainPage}>
+    <div className={cn.firstBlockWrapper}>
       {renderMenuBlock()}
-      <div className={cn["title"]}>
-        <img className={cn["image"]} src={titul} alt="картинка"></img>
+      <div className={cn.title}>
+        <img className={cn.image} src={titul} alt="картинка"></img>
       </div>
     </div>
-    <div className={cn["items-wrapper"]}>
+    <div className={cn.itemsWrapper}>
       <ContentItem
         name="Скиньскан.рф"
         imgSrc={skinscanImg}
@@ -54,8 +50,8 @@ export const MainPage: React.FunctionComponent = (props) => (
 );
 
 const renderMenuBlock = () => (
-  <div className={cn["menu"]}>
-    <div className={cn["menu-text"]}>
+  <div className={cn.menu}>
+    <div className={cn.menuText}>
       Дарина Лачихина
       <br />
       дизайнер
